4fa67cd33fa57c723f75a55a5300ab9838e4cf26
gihoon <gihoon0907@gmail.com>
2023-12-30 21:36:16 +0900

    wakent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
     const [open, setOpen] = useState(false);
     return (
         <header>
-            <a href="#"><img src="images/wak_logo.svg" alt="logo" width="70px" /></a>
+            <a href="#"><img src="https://blog.kakaocdn.net/dn/bZnhsl/btsCUzjdJvF/DFKsiOhgk86Y7hsoPyKv0k/tfile.svg" alt="logo" width="70px" /></a>
             <ul className={classNames('header_navbar', {open})}>
                 <li><a href="#about">ABOUT</a></li>
                 <li><a href="#artists">ARTISTS</a></li>
                 <li><a href="#staffs">STAFFS</a></li>
             </ul>
             <div className="header_main">
-                <a href="https://www.youtube.com/@waktaverse" className="header_ytb" target="_blank"><img src="images/youtube_icon.svg" alt="logo" width="30px" /></a>
+                <a href="https://www.youtube.com/@waktaverse" className="header_ytb" target="_blank"><img src="https://blog.kakaocdn.net/dn/bnrt2d/btsCRgSfRNn/ubDwtko9lxYRn1kNqdFgXk/tfile.svg" alt="logo" width="30px" /></a>
                 <a href="https://cafe.naver.com/steamindiegame" className="header_cafe" target="_blank"><img src="images/cafe_icon.svg" alt="logo" width="26px" /></a>
                 <div className="header_menu_icon" onClick={
                     () => {
