30cc661f42dc73fa57669df16bd9ccb5af784491
gihoon <gihoon0907@gmail.com>
2023-12-31 19:28:11 +0900

    wakent

--- src/pages/Rusuk.jsx
+++ src/pages/Rusuk.jsx
@@ -12,7 +12,7 @@ const Rusuk = () => {
             <div className="rusuk">
                 <img src="images/rusuk_profile.svg" alt="" />
                 <h1>해루석</h1>
-                <p>고정멤버 상현의 1, 왁타버스의 원빈, 해루석이 2023년 3월 발라드 가수로까지 데뷔했다..! 왁타버스의 만능 엔터테이너</p>
+                <p>고멤 상현의 1, 왁타버스의 원빈, 해루석이 2023년 3월 발라드 가수로까지 데뷔했다..! 왁타버스의 만능 엔터테이너</p>
             </div>
             <div className="rusuk_album">
                 <a href="https://www.melon.com/album/detail.htm?albumId=11210617" target="_blank"><img src="images/rusuk_album.webp" alt="" width="300px" /></a>
